fbbd29698ef62f76b044f135e4e367e41b425793
vatravanix <ecimamura@gmail.com>
2025-06-08 19:24:33 +0900

    Update to include playGame function
    Function hardcoded to 5 rounds via repeated if/else if "loops", which
    check each round for ties and repeat the round if tie is found. Also
    checks for win conditions each time in case. Understand that it's
    impossible for humanWin or computerWin to trigger at any stage earlier
    than round 5 (and would require 5 straight wins in a row for either) but
    included in each round in order to cement learning.

--- javascript.js
+++ javascript.js
@@ -63,3 +63,71 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
+// the playGame function enables the above playRound function to cycle through a full 5-round game to determine the overall winner. There are win condition alerts set for all four different outcomes: human wins 5 in 5, computer wins 5 in 5, human wins with majority within 5 rounds, computer wins with majority within 5 rounds. Ties within rounds are also handled as non-counting round and enable the game to loop that same round.  //
+
+
+function playGame() {
+    // round 1 //
+    let result = playRound(getHumanChoice(), getComputerChoice());
+    if (result === "tie") {
+        result = playRound(getHumanChoice(), getComputerChoice());
+    } else if (result === "humanWin") {
+        alert(`...and the humans clinch this 5-round extravaganza! Congratulations, you're the winner!`);
+        return;
+    } else if (result === "computerWin") {
+        alert(`...and they told us not to worry about the robot revolution. I'm sorry, but the computer's have taken the victory. Better luck next time!`);
+        return;
+    }
+    // round 2 //
+    result = playRound(getHumanChoice(), getComputerChoice());
+    if (result === "tie") {
+        result = playRound(getHumanChoice(), getComputerChoice());
+    } else if (result === "humanWin") {
+        alert(`...and the humans clinch this 5-round extravaganza! Congratulations, you're the winner!`);
+        return;
+    } else if (result === "computerWin") {
+        alert(`...and they told us not to worry about the robot revolution. I'm sorry, but the computer's have taken the victory. Better luck next time!`);
+        return;
+    }
+    // round 3 //
+    result = playRound(getHumanChoice(), getComputerChoice());
+    if (result === "tie") {
+        result = playRound(getHumanChoice(), getComputerChoice());
+    } else if (result === "humanWin") {
+        alert(`...and the humans clinch this 5-round extravaganza! Congratulations, you're the winner!`);
+        return;
+    } else if (result === "computerWin") {
+        alert(`...and they told us not to worry about the robot revolution. I'm sorry, but the computer's have taken the victory. Better luck next time!`);
+        return;
+    }
+    // round 4 //
+    result = playRound(getHumanChoice(), getComputerChoice());
+    if (result === "tie") {
+        result = playRound(getHumanChoice(), getComputerChoice());
+    } else if (result === "humanWin") {
+        alert(`...and the humans clinch this 5-round extravaganza! Congratulations, you're the winner!`);
+        return;
+    } else if (result === "computerWin") {
+        alert(`...and they told us not to worry about the robot revolution. I'm sorry, but the computer's have taken the victory. Better luck next time!`);
+        return;
+    }
+    // round 5 //
+    result = playRound(getHumanChoice(), getComputerChoice());
+    if (result === "tie") {
+        result = playRound(getHumanChoice(), getComputerChoice());
+    } else if (result === "humanWin") {
+        alert(`...and the humans clinch this 5-round extravaganza! Congratulations, you're the winner!`);
+        return;
+    } else if (result === "computerWin") {
+        alert(`...and they told us not to worry about the robot revolution. I'm sorry, but the computer's have taken the victory. Better luck next time!`);
+        return;
+    }
+    
+// this is the part that checks who wins by majority if all 5 rounds played and no-one won all 5 in a row //
+    
+    if (humanScore > computerScore) {
+        alert(`After 5 long, bloody rounds, we have a winner by majority! Drumroll please... it's you! The humans have done it, with a final score of ${humanScore} to ${computerScore}! Time to celebrate with a fleshy hug and a glass of the robot's deadliest enemy: water!`);
+    } else if (humanScore < computerScore) {
+        alert(`After 5 long, bloody rounds, we have a winner by majority! Drumroll please... ah, forget about that, just get to the bunker, quick! The robots have won, with a final score of ${computerScore} to ${humanScore}! The robot uprising is upon us!`);
+    }
+}
