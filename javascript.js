4fe47b220a6e7cc0b241aa646e41a5bd459f17f7
vatravanix <ecimamura@gmail.com>
2025-06-08 13:42:46 +0900

    Complete baseline code for getComputerChoice
    Complete basic version of getComputerChoice function coding with space
    to tighten it up later. Tested in console.log and confirmed operation.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!");
+function getComputerChoice() {
+    let x = Math.random();
+    if (x <= 0.333) {
+        return "rock";
+    } else if (x > 0.333 && x <= 0.666) {
+        return "scissors";
+    } else {
+        return "paper";
+}
+}
+
+console.log(getComputerChoice());
