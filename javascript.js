e8420a4c3ea130fa39eb460b6017c437f63143e4
vatravanix <ecimamura@gmail.com>
2025-06-08 21:49:14 +0900

    Fixed 2 bugs: double-ties and double win message
    1. In instances when rounds were tied twice in a row, they were counting
    as a completed round, ending in final scores that did not sum 5 (eg.
    human 3 - computer 1). This is fixed by a) the addition of the validRounds
    variable being counted b) the ifTieResult function allowing for
    recursion loop so that the code does not just continue on after running
    the playRound function only once after a tie and c) the finalRounds
    recursive loop repeating if the the validRounds variable != 5.
    
    2. In the (albeit rare) event that either human or computer scored 5
    wins in a row, both the specific 5-round sweep win alerts and the "win
    by majority" alerts played. This is fixed by changing of the win alerts
    so that they are only displayed whem human or computer score != 5.

--- javascript.js
+++ javascript.js
@@ -28,6 +28,7 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+let validRounds = 0;
 
 //playRound is where the answers from both getComputerChoice and getHumanChoice will be compared to one another to determine a winner. There’s a final check at the end for if anyone has reached the winning score of “5”, and instructions to carry on up to a maximum of 5 rounds if not. //
 
@@ -38,21 +39,27 @@ function playRound(humanChoice, computerChoice) {
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
         alert("Bang! Rock bashes scissors - you win this round!");
         humanScore++;
+        validRounds++;
     } else if (humanChoice === "rock" && computerChoice === "paper") {
         alert("Oh no, paper wraps rock - you lose this round!");
         computerScore++;
+        validRounds++;
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
         alert("Ouch, scissors bash rock - you lose this round!");
         computerScore++;
+        validRounds++;
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
         alert("Slice! Scissors cut paper - you win this round!");
         humanScore++;
+        validRounds++;
     } else if (humanChoice === "paper" && computerChoice === "rock") {
         alert("Crunch! Paper wraps rock - you win this round!");
         humanScore++;
+        validRounds++;
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
         alert("Damn, scissors slice paper - you lose this round!");
         computerScore++;
+        validRounds++;
     }
     alert(`Now the scores are You ${humanScore} - Computer ${computerScore}!`);
 
@@ -63,15 +70,23 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
+// ifTieResult function is to stop the problem of double-tie rounds counting as a round //
+
+function ifTieResult () {
+    let result = playRound(getHumanChoice(), getComputerChoice());
+    if (result === "tie") {
+        return ifTieResult();
+    }
+    return result;
+}
+
 // the playGame function enables the above playRound function to cycle through a full 5-round game to determine the overall winner. There are win condition alerts set for all four different outcomes: human wins 5 in 5, computer wins 5 in 5, human wins with majority within 5 rounds, computer wins with majority within 5 rounds. Ties within rounds are also handled as non-counting round and enable the game to loop that same round.  //
 
 
 function playGame() {
     // round 1 //
-    let result = playRound(getHumanChoice(), getComputerChoice());
-    if (result === "tie") {
-        result = playRound(getHumanChoice(), getComputerChoice());
-    } else if (result === "humanWin") {
+    let result = ifTieResult();
+    if (result === "humanWin") {
         alert(`...and the humans clinch this 5-round extravaganza! Congratulations, you're the winner!`);
         return;
     } else if (result === "computerWin") {
@@ -79,10 +94,8 @@ function playGame() {
         return;
     }
     // round 2 //
-    result = playRound(getHumanChoice(), getComputerChoice());
-    if (result === "tie") {
-        result = playRound(getHumanChoice(), getComputerChoice());
-    } else if (result === "humanWin") {
+    result = ifTieResult();
+    if (result === "humanWin") {
         alert(`...and the humans clinch this 5-round extravaganza! Congratulations, you're the winner!`);
         return;
     } else if (result === "computerWin") {
@@ -90,10 +103,8 @@ function playGame() {
         return;
     }
     // round 3 //
-    result = playRound(getHumanChoice(), getComputerChoice());
-    if (result === "tie") {
-        result = playRound(getHumanChoice(), getComputerChoice());
-    } else if (result === "humanWin") {
+    result = ifTieResult();
+    if (result === "humanWin") {
         alert(`...and the humans clinch this 5-round extravaganza! Congratulations, you're the winner!`);
         return;
     } else if (result === "computerWin") {
@@ -101,33 +112,37 @@ function playGame() {
         return;
     }
     // round 4 //
-    result = playRound(getHumanChoice(), getComputerChoice());
-    if (result === "tie") {
-        result = playRound(getHumanChoice(), getComputerChoice());
-    } else if (result === "humanWin") {
+    result = ifTieResult();
+    if (result === "humanWin") {
         alert(`...and the humans clinch this 5-round extravaganza! Congratulations, you're the winner!`);
         return;
     } else if (result === "computerWin") {
         alert(`...and they told us not to worry about the robot revolution. I'm sorry, but the computer's have taken the victory. Better luck next time!`);
         return;
     }
-    // round 5 //
-    result = playRound(getHumanChoice(), getComputerChoice());
-    if (result === "tie") {
-        result = playRound(getHumanChoice(), getComputerChoice());
-    } else if (result === "humanWin") {
+    // round 5 - created a function here to ensure that only these victory messages display in event of a non-majority-win and not both //
+    result = ifTieResult();
+    function finalRound() {
+    if (result === "humanWin") {
         alert(`...and the humans clinch this 5-round extravaganza! Congratulations, you're the winner!`);
         return;
     } else if (result === "computerWin") {
         alert(`...and they told us not to worry about the robot revolution. I'm sorry, but the computer's have taken the victory. Better luck next time!`);
         return;
     }
+    }
+    finalRound();
+
+    if (validRounds < 5) {
+        finalRound();
+    }
+
     
 // this is the part that checks who wins by majority if all 5 rounds played and no-one won all 5 in a row //
     
-    if (humanScore > computerScore) {
+    if (humanScore > computerScore && humanScore != 5) {
         alert(`After 5 long, bloody rounds, we have a winner by majority! Drumroll please... it's you! The humans have done it, with a final score of ${humanScore} to ${computerScore}! Time to celebrate with a fleshy hug and a glass of the robot's deadliest enemy: water!`);
-    } else if (humanScore < computerScore) {
+    } else if (humanScore < computerScore && computerScore != 5) {
         alert(`After 5 long, bloody rounds, we have a winner by majority! Drumroll please... ah, forget about that, just get to the bunker, quick! The robots have won, with a final score of ${computerScore} to ${humanScore}! The robot uprising is upon us!`);
     }
 }
