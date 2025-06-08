b7dadc14d59a7953bedd3e0eb89249c5e5bdfbef
vatravanix <ecimamura@gmail.com>
2025-06-08 19:16:27 +0900

    Complete getHumanChoice function for player input
    Add in function to prompt the human player for their choice, thereby
    representing what will eventually be the first stage of the game (at
    least in terms of what the player can visually see).

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+// getComputerChoice purpose is to randomly select between rock, paper, scissors, in order to compare this in the future against the player choice //
+
 function getComputerChoice() {
     let x = Math.random();
     if (x <= 0.333) {
@@ -6,7 +8,19 @@ function getComputerChoice() {
         return "scissors";
     } else {
         return "paper";
-}
+    }
 }
 
-console.log(getComputerChoice());
+//getHumanChoice uses the prompt to get the human to provide a (hopefully) valid answer in rock, paper, or scissors. .toLowerCase() is used to always push whatever version is entered (eg. rOCK or SCIssorS) into lower case to make comparison later possible //
+
+function getHumanChoice() {
+    const answer = prompt(`Rock, Paper, Scissors.. make your 
+    choice!`).toLowerCase();
+    if (answer !== "rock" && answer !== "paper" && answer !== "scissors") {
+        alert("Sorry, it's gotta be rock, paper, or scissors this time around! Lightning, gun, and 'God' all coming in future DLC!");
+        return getHumanChoice();
+    } else {
+    return answer;
+    }
+}
+
