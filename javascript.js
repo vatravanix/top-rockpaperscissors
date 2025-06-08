9ed502cc46eaec8329bccffe611cc5a056b63124
vatravanix <ecimamura@gmail.com>
2025-06-08 19:20:50 +0900

    Complete code for playRound function
    Add in the playRound function which holds the basis of the game. Choice
    was made to stick as close to what TOP has explicitly taught/advised
    about, therefore no include of for/while, do, or loops at this stage.

--- javascript.js
+++ javascript.js
@@ -29,3 +29,37 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+//playRound is where the answers from both getComputerChoice and getHumanChoice will be compared to one another to determine a winner. There’s a final check at the end for if anyone has reached the winning score of “5”, and instructions to carry on up to a maximum of 5 rounds if not. //
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        alert("It's a tie! Let's try again!");
+        return "tie";
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        alert("Bang! Rock bashes scissors - you win this round!");
+        humanScore++;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        alert("Oh no, paper wraps rock - you lose this round!");
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        alert("Ouch, scissors bash rock - you lose this round!");
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        alert("Slice! Scissors cut paper - you win this round!");
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        alert("Crunch! Paper wraps rock - you win this round!");
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        alert("Damn, scissors slice paper - you lose this round!");
+        computerScore++;
+    }
+    alert(`Now the scores are You ${humanScore} - Computer ${computerScore}!`);
+
+    if (humanScore === 5) {
+        return "humanWin";
+    } else if (computerScore === 5) {
+        return "computerWin";
+    }
+}
+
